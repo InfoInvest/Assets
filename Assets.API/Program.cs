ece87380de814fb711ab83fa51a83909b94de3f0
João Xavier <fastjonh@gmail.com>
2022-05-22 20:13:08 -0300

    main

--- Assets.API/Program.cs
+++ Assets.API/Program.cs
@@ -1,38 +1,18 @@
 using Core.API;
 
-static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndPointDefinitions(typeof(Program));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-    builder.Services.AddEndPointDefinitions(typeof(Program));
-
-    var app = builder.Build();
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseEndpointDefinitions();
-    app.Run();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddEndPointDefinitions(typeof(Program));
-
-//var app = builder.Build();
-
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseEndpointDefinitions();
-//app.Run();
+app.UseEndpointDefinitions();
+app.Run();
