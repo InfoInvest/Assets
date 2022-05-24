50c7b762139a8bcaffed8c5ff4d0da1bec36167c
João Xavier <fastjonh@gmail.com>
2022-05-23 22:04:09 -0300

    using infra ef nuget

--- Assets.Infra/AssetRepositoryEF.cs
+++ Assets.Infra/AssetRepositoryEF.cs
@@ -2,29 +2,10 @@
 
 namespace Assets.Infra
 {
-    public class AssetRepositoryEF : IAssetRepository
+    public class AssetRepositoryEF : RepositoryEF<Asset>, IAssetRepository
     {
-        private readonly AssetsDbContext _context;
-
-        public AssetRepositoryEF(AssetsDbContext context)
+        public AssetRepositoryEF(AssetsDbContext context) : base(context)
         {
-            _context = context;
-        }
-
-        public void Add(Asset entity)
-        {
-            _context.Assets.Add(entity);
-            _context.SaveChanges();
-        }
-
-        public Asset? GetById(Guid id)
-        {
-            return _context.Assets.Find(id);
-        }
-
-        public List<Asset> GetRange(int start, int end)
-        {
-            return _context.Assets.Skip(start).Take(end).ToList();
         }
     }
 }
